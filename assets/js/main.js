aabc74e4984af815b4063435fffe324619e65373
Daz Johnson <97320359+Dazjanae@users.noreply.github.com>
2022-05-09 18:10:49 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+document.getElementById('tan').onclick = paintTan
+document.getElementById('brown').onclick = paintBrown
+document.getElementById('blue').onclick = paintBlue
+document.getElementById('black').onclick = paintBlack
+document.getElementById('peach').onclick = paintPeach
+document.getElementById('lavendar').onclick = paintLavendar
+
+function paintTan() {
+  document.querySelector('body').style.backgroundColor = 'rgba(219, 192, 171)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function paintBrown() {
+  document.querySelector('body').style.backgroundColor = 'rgba(110, 80, 71)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function paintBlue(){
+  document.querySelector('body').style.backgroundColor = 'rgba(190, 232, 231)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function paintBlack() {
+  document.querySelector('body').style.backgroundColor = 'rgba(83, 83, 84)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function paintPeach() {
+  document.querySelector('body').style.backgroundColor = 'rgba(247, 192, 173)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function paintLavendar(){
+  document.querySelector('body').style.backgroundColor = 'rgba(242, 227, 255)'
+  document.querySelector('body').style.color = 'white'
+}
